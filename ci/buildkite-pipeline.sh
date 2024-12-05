ea49917007c63dde98bd6ed612e4e38ba8793ea5
Nolan Le <9296903+nolanle@users.noreply.github.com>
2024-12-05 17:37:12 +0700

    chore(ci): add buildkite pipelines (#4)
    * chore: added buildkite config
    
    * chore: add exec permission ci/buildkite-pipeline.sh
    
    * chore(ci): update ci/buildkite-pipeline.sh
    
    * chore(ci): update ci/buildkite-pipeline.sh
    
    * chore(ci): update ci/buildkite-pipeline.sh

---
+++ ci/buildkite-pipeline.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# Builds a buildkite pipeline based on the environment variables
+#
+
+set -e
+cd "$(dirname "$0")"/..
+
+output_file=${1:-/dev/stderr}
+
+if [[ -n $CI_PULL_REQUEST ]]; then
+  IFS=':' read -ra affected_files <<< "$(buildkite-agent meta-data get affected_files)"
+  if [[ ${#affected_files[*]} -eq 0 ]]; then
+    echo "Unable to determine the files affected by this PR"
+    exit 1
+  fi
+else
+  affected_files=()
+fi
+
+start_pipeline() {
+  echo "# $*" > "$output_file"
+  echo "steps:" >> "$output_file"
+}
+
+wait_step() {
+  echo "  - wait" >> "$output_file"
+}
+
+start_pipeline "Push pipeline for ${BUILDKITE_BRANCH:-?unknown branch?}"
+wait_step
+exit 0
